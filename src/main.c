4ba1a3d19fad0dd9e56be9ce91f167273ccc2302
progof <34421475+progof@users.noreply.github.com>
2023-02-16 14:28:29 +0100

    Added counter of collected bones

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <time.h>
 
 int main()
 {
@@ -11,7 +12,10 @@ int main()
     short x_dog = 8, y_dog = 5;
     short ox, oy;
     short x_bone = 3, y_bone = 3;
+    short score = 0;
     char key;
+
+    srand(time(NULL));
     
     do{
     // Create a field    
@@ -27,6 +31,8 @@ int main()
     field[x_bone][y_bone] = '*';
 
     system("clear");
+    printf("\n\nScore: %hd\n\n",score);
+    printf("'w'-> up | 's' -> down | 'a' -> left | 'd' -> right | 'e' -> exit\n\n");
     for(int i = 0; i < 10; i++)
     {
         printf("%s\n",field[i]);
@@ -41,11 +47,20 @@ int main()
     if (key == 's') x_dog++;
     if (key == 'a') y_dog--;
     if (key == 'd') y_dog++;
+
     if (field[x_dog][y_dog] == '#')
     {
         x_dog = ox;
         y_dog = oy;
     }
+
+    if ((x_dog == x_bone) && (y_dog == y_bone))
+    {
+        y_bone = rand() * 1.0 / RAND_MAX * 18 + 1;
+        x_bone = rand() * 1.0 / RAND_MAX * 8 + 1;
+        score++;
+    }
+    
     
 
     }while(key != 'e');
