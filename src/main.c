06db0f47bc190cd4468e871d1106990e20ea3fc6
progof <34421475+progof@users.noreply.github.com>
2023-02-16 14:09:08 +0100

    Added keyboard control and bone

--- src/main.c
+++ src/main.c
@@ -8,41 +8,48 @@
 int main()
 {
     char field[10][21] = {0};
-    int x_dog = 8, y_dog = 5;
+    short x_dog = 8, y_dog = 5;
+    short ox, oy;
+    short x_bone = 3, y_bone = 3;
     char key;
     
+    do{
+    // Create a field    
     sprintf(field[0], "####################");
     for (int i = 1; i < 9; i++){
         sprintf(field[i], "#                  #");
     }       
     sprintf(field[9], "####################");
-
+    
+    //Create a dog
     field[x_dog][y_dog] = '@';
+    //Create a bone
+    field[x_bone][y_bone] = '*';
 
+    system("clear");
     for(int i = 0; i < 10; i++)
     {
         printf("%s\n",field[i]);
     }
 
+    // Keyboard control
     key = getchar();
 
-    if (key == 'w') y_dog--;
-    if (key == 's') y_dog++;
-    if (key == 'a') x_dog--;
-    if (key == 'd') x_dog++;
+    ox = x_dog;
+    oy = y_dog;
+    if (key == 'w') x_dog--;
+    if (key == 's') x_dog++;
+    if (key == 'a') y_dog--;
+    if (key == 'd') y_dog++;
+    if (field[x_dog][y_dog] == '#')
+    {
+        x_dog = ox;
+        y_dog = oy;
+    }
+    
+
+    }while(key != 'e');
     
 
     return 0;
-}
-
-/*
-for (int i = 0; i < 10; i++)
-    {
-        for (int k = 0; k < 21; k++)
-        {
-            field[i][k] = '#';
-            printf("%c", field[i][k]);
-        }
-        
-    }
- */   
+}  
